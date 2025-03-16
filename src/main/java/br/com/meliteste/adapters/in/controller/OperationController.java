d6aafc08c5b42c52447169612504a7aa8c4f3719
Daniel Fernando da Silva de Castro <daniel.fs.castro@gmail.com>
2025-03-16 17:24:32 -0300

    collection and list method for test

--- src/main/java/br/com/meliteste/adapters/in/controller/OperationController.java
+++ src/main/java/br/com/meliteste/adapters/in/controller/OperationController.java
@@ -18,6 +18,12 @@ import org.springframework.retry.annotation.Retryable;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 @RestController
 @RequestMapping(path = "/api/v1/operation", produces = {MediaType.APPLICATION_JSON_VALUE})
 @Validated
@@ -37,16 +43,61 @@ public class OperationController {
             maxAttempts = 3,
             backoff = @Backoff(delay = 2000)
     )
-    public ResponseEntity<ResponseDto> createOperation(@RequestBody OperationRequest operationRequest) {
-        System.out.println(operationRequest);
-        Operation operation = operationRequestMapper.toOperation(operationRequest);
+//    public ResponseEntity<ResponseDto> createOperation(@RequestBody OperationRequest operationRequest) {
+//        System.out.println(operationRequest);
+//        Operation operation = operationRequestMapper.toOperation(operationRequest);
+//
+//        executeGatewaysInputPort.execute(OperationType.fromValue(operation.getName()), operation);
+//
+//        insertOperationInputPort.save(operation);
+//
+//        return buildResponseEntity(HttpStatus.CREATED, HttpStatus.CREATED.value() + "", "Operação criada com sucesso");
+//
+//    }
+    public ResponseEntity<ResponseDto> processOperations(@RequestBody List<OperationRequest> operationRequests) {
+        ExecutorService executorService = Executors.newFixedThreadPool(10); // Define um pool de threads
 
-        executeGatewaysInputPort.execute(OperationType.fromValue(operation.getName()), operation);
+        try {
+            // Cria uma lista de tarefas usando Callable
+            List<Callable<Void>> tasks = operationRequests.stream()
+                    .map(operationRequest -> (Callable<Void>) () -> {
+                        System.out.println(operationRequest);
+                        Operation operation = operationRequestMapper.toOperation(operationRequest);
+                        executeGatewaysInputPort.execute(OperationType.fromValue(operation.getName()), operation);
+                        insertOperationInputPort.save(operation);
+                        return null; // Callable precisa retornar algo; apenas retornamos null.
+                    })
+                    .toList();
 
-        insertOperationInputPort.save(operation);
+            // Executa todas as tarefas e aguarda a conclusão
+            List<Future<Void>> futures = executorService.invokeAll(tasks);
 
-        return buildResponseEntity(HttpStatus.CREATED, HttpStatus.CREATED.value() + "", "Operação criada com sucesso");
+            // Opcional: Verifica se todas as tarefas foram concluídas com sucesso
+            for (Future<Void> future : futures) {
+                try {
+                    future.get(); // Se houver uma exceção, será lançada aqui
+                } catch (Exception e) {
+                    Throwable cause = e.getCause();
+                    if (cause instanceof ClientResponseStatusException) {
+                        // Logue detalhes específicos do erro
+                        ClientResponseStatusException clientException = (ClientResponseStatusException) cause;
+                        System.err.println("Erro detalhado: " + clientException.getMessage());
+                    }
+                    // Lidar com exceção durante a execução da tarefa
+                    System.err.println("Erro ao processar operação: " + cause);
+                    cause.printStackTrace();
 
+                }
+            }
+
+            return buildResponseEntity(HttpStatus.CREATED, HttpStatus.CREATED.value() + "", "Operações criadas com sucesso.");
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); // Restaura o estado de interrupção
+            return buildResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR.value() + "" ,"Erro ao processar operações");
+        } finally {
+            executorService.shutdown(); // Certifique-se de desligar o executor
+        }
     }
 
     @GetMapping("/fetch")
